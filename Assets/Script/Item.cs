e36697255a4bbb1617e26b8875d14d5a74a11659
Bryan Alvarado <bryan.alvarado@gmail.com>
2019-01-27 10:41:56 -0600

    Add logic to return item to item script

--- Assets/Script/Item.cs
+++ Assets/Script/Item.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using Cinemachine;
 
 public class Item : MonoBehaviour
 {
@@ -11,6 +12,14 @@ public class Item : MonoBehaviour
 
     [SerializeField]
     private Type itemType;
+    [SerializeField]
+    private SpriteRenderer spriteToActivate;
+    [SerializeField]
+    private Explodable objectToExplode;
+    [SerializeField]
+    private CinemachineVirtualCamera cameraToTransition;
+
+    private bool isInDroppingArea;
 
     Rigidbody2D rigidbody2D;
     SpriteRenderer spriteRenderer;
@@ -36,8 +45,31 @@ public class Item : MonoBehaviour
         }
     }
 
+    public void ReturnItem()
+    {
+        spriteToActivate.enabled = true;
+        objectToExplode.explode();
+        cameraToTransition.gameObject.SetActive(true);
+        transform.parent.GetComponent<PlayerController2D>().isCarryingItem = false;
+        Invoke("DeactivateCamera", 6f);
+    }
+
+    private void DeactivateCamera()
+    {
+        cameraToTransition.gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+
     private void Explode()
     {
         GetComponent<Explodable>().explode();
+    }
+
+    private void OnTriggeEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("DroppingArea"))
+        {
+            ReturnItem();
+        }
     }
 }
